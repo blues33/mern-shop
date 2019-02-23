88d2411f44998906486083b86d797566d1a61714
Alexander <wondroussquirrel@gmail.com>
2019-02-23 19:44:43 +0200

    sing in style

--- client/auth/Components/SingIn.jsx
+++ client/auth/Components/SingIn.jsx
@@ -3,22 +3,31 @@ import React, { useEffect } from "react";
 import Input from "../../Components/Input";
 
 const SingIn = () => {
-  document.bgColor = "#BFD9F2";
+  document.bgColor = "#BFD9F2"; /* pale aqua */
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
 
   return (
     <div className="wrapper">
       <div className="form-wrapper">
         <h1>Вход</h1>
         <hr />
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">
-            Email
-            <Input name="email" placeholder="email" />
+            Почта
+            <Input name="email" placeholder="почта" />
           </label>
           <label htmlFor="password">
-            Password
-            <Input name="password" placeholder="password" />
+            Пароль
+            <Input name="password" placeholder="пароль" />
           </label>
+
+          <button type="submit">
+            <p>Войти</p>
+          </button>
+          <small>Не зарегистрированы?</small>
         </form>
       </div>
     </div>
--- webpack.common.js
+++ webpack.common.js
@@ -60,6 +60,17 @@ module.exports = {
         ]
       },
       {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           "file-loader",
